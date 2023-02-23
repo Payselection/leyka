4a0ecf1883b87b0b91d08a469f7544c5422e6334
Rita Tapero <rita.tapero@solbeg.com>
2023-02-23 10:35:50 +0300

    webhook test

--- gateways/payselection/lib/Payselection_Merchant_Api.php
+++ gateways/payselection/lib/Payselection_Merchant_Api.php
@@ -187,11 +187,27 @@ class Payselection_Merchant_Api
             );
         }
         
+
+        $file = get_template_directory() . '/payselection-webhook.txt'; 
+        $current = file_get_contents($file);
+        $current .= "$_SERVER[REQUEST_METHOD] = ".$_SERVER['REQUEST_METHOD']."\n\n\n\n";
+        $open = file_put_contents($file, $current);
+
+
+
         // Check signature
         //$request_method = isset($_SERVER['REQUEST_METHOD']) ? sanitize_text_field(wp_unslash($_SERVER['REQUEST_METHOD'])) : '';
         $request_method = 'POST';
         $signBody = $request_method . PHP_EOL . home_url('/leyka/service/payselection/response') . PHP_EOL . $site_id . PHP_EOL . json_encode($request);
         $signCalculated = self::getSignature($signBody, $secret_key);
+
+        $file = get_template_directory() . '/payselection-webhook.txt'; 
+        $current = file_get_contents($file);
+        $current .= "signBody = ".$signBody."\n\n\n\n";
+        $current .= "signCalculated = ".$signCalculated."\n\n\n\n";
+        $current .= "headers[X-WEBHOOK-SIGNATURE] = ".$headers['X-WEBHOOK-SIGNATURE']."\n\n\n\n";
+        $open = file_put_contents($file, $current);
+
 
         if ($headers['X-WEBHOOK-SIGNATURE'] !== $signCalculated) {
             return new \WP_Error(
